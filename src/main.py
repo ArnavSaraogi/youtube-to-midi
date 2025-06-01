cec11c19bf337fbe99c608234a4b0a6c97e6cdbf
arnsar <arnavsaraogi1@gmail.com>
2025-06-01 00:15:41 -0400

    idek bruh

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import sheet_music
 import cv2 as cv
 import numpy as np
 
-song = 'howl'
+song = 'la la'
 url = ''
 start = 0
 end = 0
@@ -25,7 +25,7 @@ fps, total_frames = video_processing.get_frame_info(video_path, duration)
 start_frame, end_frame = video_processing.get_start_and_end_frames(fps, total_frames, start, end)
 gray_first_frame, hsv_first_frame = video_processing.get_first_frame(video_path, start_frame)
 
-#piano analysis
+# piano analysis
 gray_first_frame, crop_line_y = piano_analysis.crop_to_piano(gray_first_frame)
 hsv_first_frame = hsv_first_frame[crop_line_y:]
 key_rois = piano_analysis.locate_keys(gray_first_frame, hsv_first_frame)
@@ -34,7 +34,7 @@ note_matrix, pressed_colors = piano_analysis.make_note_matrix(video_path, crop_l
 print("machine: learning...")
 hand_assignments = piano_analysis.get_hands(pressed_colors)
 
-debug.visualize_note_matrix(
+"""debug.visualize_note_matrix(
     video_path=video_path,
     crop_line_y=crop_line_y,
     start_frame=start_frame,
@@ -42,8 +42,9 @@ debug.visualize_note_matrix(
     note_matrix=note_matrix,
     key_rois=key_rois,
     hand_assignments=hand_assignments
-)
+)"""
 
 # sheet music engraving
 events_left_hand, events_right_hand = sheet_music.matrix_to_events(note_matrix, hand_assignments, fps)
-sheet_music.generate_midi(events_left_hand, events_right_hand)
+midi_path = sheet_music.generate_midi(events_left_hand=events_left_hand, events_right_hand=events_right_hand)
+sheet_music.midi_to_sheet(midi_path=midi_path, time_signature="3/4")
--- src/sheet_music.py
+++ src/sheet_music.py
@@ -1,4 +1,8 @@
 import pretty_midi
+import music21
+import mido
+import os
+import copy
 
 # puts events in format (note, start, duration)
 def matrix_to_events(note_matrix, hand_assignments, fps):
@@ -30,7 +34,7 @@ def matrix_to_events(note_matrix, hand_assignments, fps):
     
     return (events_left_hand, events_right_hand)
 
-def generate_midi(events_left_hand, events_right_hand, output_path="output.mid", velocity=80):
+def generate_pretty_midi(events_left_hand, events_right_hand, velocity):
     pm = pretty_midi.PrettyMIDI()
     
     left_hand = pretty_midi.Instrument(program=0, name="Left Hand")
@@ -60,4 +64,72 @@ def generate_midi(events_left_hand, events_right_hand, output_path="output.mid",
     pm.instruments.append(left_hand)
     pm.instruments.append(right_hand)
 
-    pm.write(output_path)
+    return pm
+
+def generate_midi(events_left_hand, events_right_hand, bpm=120, velocity=80, output_path="output.mid"):
+    pm = generate_pretty_midi(events_left_hand, events_right_hand, velocity)
+    temp_path = "temp.mid"
+    pm.write(temp_path)
+    
+    mid = mido.MidiFile(temp_path)
+    tempo_track = mido.MidiTrack()
+
+    tempo = mido.bpm2tempo(bpm)
+    tempo_msg = mido.MetaMessage('set_tempo', tempo=tempo, time=0)
+    tempo_track.append(tempo_msg)
+
+    mid.tracks.insert(0, tempo_track)
+
+    os.remove(temp_path)
+    mid.save(output_path)
+    return output_path
+
+def midi_to_sheet(midi_path="output.mid", key_signature=None, time_signature="4/4", output_path="output.musicxml"):
+    score = music21.converter.parse(midi_path)
+    grand_score = music21.stream.Score()
+
+    if len(score.parts) < 2:
+        raise ValueError("MIDI file must have at least two parts (left and right hand).")
+
+    left_notes = score.parts[0].flatten().notes
+    right_notes = score.parts[1].flatten().notes
+
+    if key_signature is None:
+        key = score.analyze('key')
+        key_signature = key.sharps
+    
+    ks = music21.key.KeySignature(key_signature)
+    ts = music21.meter.TimeSignature(time_signature)
+
+    left_hand = music21.stream.Part()
+    right_hand = music21.stream.Part()
+
+    # Insert instruments and clefs into each part at offset 0
+    left_hand.insert(0, music21.instrument.Piano())
+    left_hand.insert(0, music21.clef.BassClef())
+    left_hand.insert(0, ks)
+    left_hand.insert(0, ts)
+
+    right_hand.insert(0, music21.instrument.Piano())
+    right_hand.insert(0,music21.clef.TrebleClef())
+    right_hand.insert(0, ks)
+    right_hand.insert(0, ts)
+
+    for n in left_notes:
+        left_hand.append(n)
+    for n in right_notes:
+        right_hand.append(n)
+
+    # Add parts to grand score
+    grand_score.insert(0, right_hand)
+    grand_score.insert(0, left_hand)
+    
+    # Add brace to group both staves
+    grand_score.insert(0, music21.layout.StaffGroup(
+        [left_hand, right_hand],
+        name='Piano',
+        abbreviation='Pno.',
+        symbol='brace'
+    ))
+
+    grand_score.write('musicxml', output_path)
